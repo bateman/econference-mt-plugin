efb3497bc4dd15dc680577cb9d866e51054cf54f
calefato.uniba@gmail.com <calefato.uniba@gmail.com@789cd387-81a1-5243-9504-ffee8a4352e0>
2012-03-29 17:06:14 +0000

    Added test after upgrade to new Google Translate API (paid) version

--- org.apertium.api.translate.test/src/org/apertium/api/translate/internal/test/TranslatorTest.java
+++ org.apertium.api.translate.test/src/org/apertium/api/translate/internal/test/TranslatorTest.java
@@ -1,26 +1,51 @@
 package org.apertium.api.translate.internal.test;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
+import net.sf.okapi.connectors.google.GoogleMTv2Connector;
+import net.sf.okapi.connectors.google.GoogleMTv2Parameters;
 
-import java.net.MalformedURLException;
-
-import org.junit.Test;
 import org.apertium.api.exceptions.ApertiumXMLRPCClientException;
 import org.apertium.api.translate.Translator;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
 
 public class TranslatorTest {
+	Translator translator = null;
+
+	@Before
+	public void initialize() {
+		translator = new Translator();
+	}
 
 	@Test
 	public void testTranslator() {
-		Translator translator = null;
-		try {
-			translator = new Translator();
-		} catch (Exception e) {
-
-		}
 		assertTrue(translator != null);
 	}
 
-	
+	@Test
+	public void testLoadMTProperties() {
+		assertNotNull(Translator.getGtApiKeyVal());
+	}
+
+	@Test
+	public void testTranslateGoogle() throws InterruptedException,
+			ApertiumXMLRPCClientException {
+		GoogleMTv2Parameters param = new GoogleMTv2Parameters();
+		param.setApiKey("AIzaSyDMoCPi857TLhqcINY8WUydTlVTLooxO4E");
+		Object connector = new GoogleMTv2Connector();
+		((GoogleMTv2Connector) connector).setParameters(param);
+		for (int i = 0; i < 100; i++) {
+			String transl = Translator._translate(
+					"tradurre il messaggio da Italiano a Inglese", "it",
+					"en", connector);
+			System.out.println(transl);
+			Assert.assertEquals(
+					"the message translated from Italian to English", transl);
+			Thread.sleep(500);
+		}
+
+	}
 
 }
